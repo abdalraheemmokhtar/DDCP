6e0316dae76f467fd7c1ddfea883352e27793c5f
Abdalraheem Mokhtar <abdalraheem.mokhtar@gmail.com>
2024-04-11 19:04:49 +0400

    "Create the file data/creature.py using sqlite3"

---
+++ src/data/creature.py
@@ -0,0 +1,11 @@
+import sqlite3
+from model.creature import Creature
+
+DB_NAME = "cryptid.db"
+conn = sqlite3.connect(DB_NAME)
+curs = conn.cursor()
+def init():
+    curs.execute("create table creature(name, description, country, area, aka)")
+def row_to_model(row: tuple) -> Creature:
+    name, description, country, area, aka = row
+    return Creature(name, description, country, area, aka)
